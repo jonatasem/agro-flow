4016a7caa94b7baa8f1861dc52cfb9f1ae020581
jonatasem <jonatas.em20@gmail.com>
2026-07-14 19:05:16 -0300

    criando controller do workOrder

---
+++ server/src/controllers/WorkOrder/CreateWorkOrderController.ts
@@ -0,0 +1,68 @@
+import prismaClient from "../../prisma/index.js";
+
+interface CreateWorkOrderProps {
+    fleet: string;
+    setor: string;
+    qruDescricao: string;
+    qth: string;
+    criadoPor: string;
+}
+
+export class CreateWorkOrderController {
+    async execute({fleet, setor, qruDescricao, qth, criadoPor}: CreateWorkOrderProps) {
+        if(!fleet || !setor || !qruDescricao || !qth || !criadoPor) {
+            throw new Error("Todos os campos são obrigatórios");
+        }
+
+        const equipment = await prismaClient.equipment.findUnique({
+            where: {fleet},
+        });
+
+        if(!equipment) {
+            throw new Error("Equipamento não encontrado");
+        }
+
+        const activeWorkOrder = await prismaClient.workOrder.findFirst({
+            where: {
+                equipmentId: equipment.id,
+                status: "ABERTA",
+            },
+        });
+
+        if(activeWorkOrder) {
+            const newSectorService = await prismaClient.sectorService.create({
+                data: {
+                    workOrderId: activeWorkOrder.id,
+                    setor,
+                    qruDescricao,
+                    qth,
+                    criadoPor,
+                    status: "Aguardando Manutenção"
+                },
+            });
+            return newSectorService;
+        }
+
+        const newWorkOrder = await prismaClient.workOrder.create({
+            data: {
+                equipmentId: equipment.id,
+                city: equipment.city,
+                status: "ABERTA ",
+                setores: {
+                    create: {
+                        setor,
+                        qruDescricao,
+                        qth,
+                        criadoPor,
+                        status: "Aguardando Manutenção",
+                    },
+                },
+            },
+            include: {
+                setores: true,
+            },
+        });
+
+        return newWorkOrder;
+    }
+}
