091a3f5f5c1e078760df8b47b17b36deacf0297c
jonatasem <jonatas.em20@gmail.com>
2026-07-15 21:22:19 -0300

    criando campo password no cadastro do collaborator, gerando um hash seguro para proteger password

--- server/src/controllers/Collaborator/CreateCollaboratorController.ts
+++ server/src/controllers/Collaborator/CreateCollaboratorController.ts
@@ -1,25 +1,33 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { CreateCollaboratorService } from "../../services/Collaborator/CreateCollaboratorService.js";
 
+interface CreateCollaboratorProps {
+  name: string;
+  role: string;
+  registration: string;
+  password: string;
+  city: string;
+}
+
 export class CreateCollaboratorController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
-    const { name, role, registration, password, city } = request.body as {
-      name: string;
-      role: string;
-      registration: string;
-      password: string;
-      city: string;
-    };
 
-    const authorizedService = new CreateCollaboratorService();
-    const authorized = await authorizedService.execute({
-      name,
-      role,
-      registration,
-      password,
-      city,
-    });
+    const { name, role, registration, password, city } = request.body as CreateCollaboratorProps;
 
-    return reply.status(201).send(authorized);
+    const collaboratorService = new CreateCollaboratorService();
+   
+    try {
+      const collaborator = await collaboratorService.execute({
+        name,
+        role,
+        registration,
+        password,
+        city,
+      });
+
+      return reply.status(201).send(collaborator);
+    } catch (error: any){
+      return reply.status(400).send({error: error.message});
+    }
   }
 }
--- server/src/services/Collaborator/CreateCollaboratorService.ts
+++ server/src/services/Collaborator/CreateCollaboratorService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma/index.js";
+import { hash } from "bcryptjs";
 
 interface CreateCollaboratorProps {
   name: string;
@@ -16,21 +17,35 @@ export class CreateCollaboratorService {
     password,
     city,
   }: CreateCollaboratorProps) {
-    if (!name || !role || !registration || !city) {
+    if (!name || !role || !registration || !password || !city) {
       throw new Error("Todos os campos são obrigatórios");
     }
+
+    const collaboratorExists = await prismaClient.collaborator.findUnique({
+      where: {
+        registration: registration
+      },
+    });
+
+    if(collaboratorExists){
+      throw new Error("Esta matrícula já está cadastrada no sistema");
+    }
+
+    const hashedPassword = await hash(password, 8);
 
     const collaborator = await prismaClient.collaborator.create({
       data: {
         name,
         role,
         registration,
-        password,
+        password: hashedPassword,
         city,
         status: true,
       },
     });
 
-    return collaborator;
+    const { password: _, ...collaboratorWithoutPassWord } = collaborator;
+
+    return collaboratorWithoutPassWord;
   }
 }
